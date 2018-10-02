2a00b2989c378ab9211ef72c21662b4acf45748b
Sharon Oh <ohs790@newschool.edu>
2018-10-01 20:04:18 -0400

    second push

---
+++ minigame2/Assets/nextscene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nextscene : MonoBehaviour {
+    public int sceneNum;
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 50, 100, 40), "Load Scene: " + sceneNum))
+        {
+            SceneManager.LoadScene(sceneNum);
+        }
+    }
+
+}
+
+
